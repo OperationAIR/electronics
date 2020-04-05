762ae412fe38dd6c47994fdf4b7879459e07fd18
Sjors de Wit <dev@sjorsdewit.name>
2020-04-05 22:13:06 +0200

    Pi comms: read sensor data atomically
    In practice this probably does not make any different, but let's read
    all sensor data in one atomic copy to be sure no values are halfway
    being updated.

--- firmware/src/pi_communication.c
+++ firmware/src/pi_communication.c
@@ -7,6 +7,7 @@
 
 #include <c_utils/ringbuffer.h>
 #include <mcu_timing/delay.h>
+#include <lpc_tools/irq.h>
 
 #include "pi_communication.h"
 
@@ -199,8 +200,14 @@ void pi_comm_tasks()
     }
 
 	if (g_current_command == PiCommandRequestSensorValues) {
+
 			SensorsAllData data;
+            // Critical section: sensor data should be read atomically.
+            // This guarantees no values are halfway being updated.
+            const bool critical_section = irq_disable();
 			sensors_read_all(&data);
+            irq_restore(critical_section);
+
 			uint16_t crc_state = 0xFFFF;
             uint16_t crc16 = crc16_usb_stream_check(&crc_state, (uint8_t*)&data, sizeof(SensorsAllData));
 			uint32_t prefix = PiCommandRequestSensorValues;
